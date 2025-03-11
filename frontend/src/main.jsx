38f55d036a2e3750795164536ab948c256c2ef12
Tokuma Abdisa <tokkiyuam@gmail.com>
2025-03-11 16:22:04 +0300

    update google.jsx and main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,10 +6,11 @@ import { GoogleOAuthProvider } from "@react-oauth/google";
 
 import { Provider } from "react-redux";
 import store from "./redux/store.js";
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-      <GoogleOAuthProvider clientId='950400040435-m6364alic1gl35r7oc17k2992cc4i6hv.apps.googleusercontent.com'>
+      <GoogleOAuthProvider clientId='278012306514-3jd4o7shq0e41s105884eqmrt5821367.apps.googleusercontent.com'>
         <App />
       </GoogleOAuthProvider>
     </Provider>
