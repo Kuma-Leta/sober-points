259bc21c740d85cf61372fd62125a43fd81499fc
murtessa <murtessajabesa65@gmail.com>
2025-03-29 11:16:20 +0300

    latest

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -25,9 +25,9 @@ import MyVenueDetail from "./pages/venues/myVenueDetail";
 import AdminAnalytics from "./pages/venues/AdminAnalytics";
 import BottomNavBar from "./pages/venues/BottomNavBar"; // Import the BottomNavBar
 import Profile from "./pages/users/profile";
-
 import VenueList from "./pages/landing/VenueList";
 import PrivacyPolicy from "./pages/landing/privacyPolicy";
+import NotFound from "./pages/404";
 
 function Pages() {
   const user = useSelector((state) => state.auth.user);
