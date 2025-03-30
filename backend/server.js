9b4253cd900a90ead34019057194f94e20b5e46e
Tokuma Abdisa <tokkiyuam@gmail.com>
2025-03-30 13:18:35 +0300

    update useAuth.js

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "http://localhost:5174",
+      "http://localhost:5174", "http://localhost:3000",
       "https://sober-frontend.onrender.com",
     ],
     credentials: true,
