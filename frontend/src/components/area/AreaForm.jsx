1f6fd9fbeace4d94bb864a2563a44b8da6142f1e
kuma <kumaleta2021@gmail.com>
2025-07-29 21:40:33 +0300

    update to api url after deploying backend

--- frontend/src/components/area/AreaForm.jsx
+++ frontend/src/components/area/AreaForm.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/api";
+const API_URL = "https://sober-points.onrender.com//api";
 
 const AreaForm = ({ onClose, onAreaCreated }) => {
   const [name, setName] = useState("");
